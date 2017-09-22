03b84898d7e0bb04e2d82c1b7b96e2ff48bf252c
Yoshihiro Shimizu <31213083+fpocket-2017@users.noreply.github.com>
2017-09-22 14:27:24 +0900

    commit

--- train/main.py
+++ train/main.py
@@ -6,8 +6,6 @@ import pickle
 from const  import *
 from gensim import models
 
-EPOCHS = 20
-
 # Load docs file
 print('Loading docs file...')
 with codecs.open('{}/{}'.format(PATH_INPUT, u'docs.bin'), 'rb') as fp:
@@ -16,7 +14,7 @@ with codecs.open('{}/{}'.format(PATH_INPUT, u'docs.bin'), 'rb') as fp:
 # Make a model
 print('Making a model...')
 model = models.Doc2Vec(
-            size      = 10,
+            size      = 400,
             alpha     = 0.0015,
             sample    = 1e-5,
             min_count = 1,
@@ -26,12 +24,10 @@ model = models.Doc2Vec(
 print('Training the model...')
 model.build_vocab(docs)
 
-print('corpus_count: {0}'.format(model.corpus_count))
-print('epochs      : {0}'.format(model.iter))
 model.train(
     docs,
     total_examples = model.corpus_count,
-    epochs         = 200
+    epochs         = 50
     )
 
 # Save the model
